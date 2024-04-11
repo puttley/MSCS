3b5dab793e3e262d1e6c0870d43e7018e87d7349
puttley <puttley@pitsco.com>
2024-04-11 09:28:30 -0500

    removed filter

--- bt.js
+++ bt.js
@@ -128,8 +128,8 @@ async function connect() {
 
     //filters:[{namePrefix: [DEVICE_BT_NAME_PREFIX]},],
 
-      acceptAllDevices:true
-    //  optionalServices: [NUS_SERVICE_UUID]
+      acceptAllDevices:true,
+      optionalServices: [NUS_SERVICE_UUID]
 
   }).then(onScanResult).catch(function(e) {
     console.warn('failed to find a device:', e);
